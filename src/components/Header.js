49ec6081b53dd352d1e9e4f119c065bc7a2f0d17
NaradaJ <140716249+NaradaJ@users.noreply.github.com>
2023-11-24 17:14:13 +0530

    buttton component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className='header'>
+        <h1>{title}</h1>
     </header>
       
-    
   )
 }
 
+Header.defaultProps = {
+    title: "Task Tracker ",
+  }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+  
+/* --> this is CSS in J
+const headingStyle = {
+    color:"red",
+    backgroundColor:'black'
+} 
+*/
 export default Header
